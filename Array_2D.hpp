db5677e8acf819e2119580a91ed69b60b9a276ab
Maciej Wędzik <logmaciej@gmail.com>
2025-07-12 20:56:31 +0200

    Initial commit: basic CMake, .gitignore

---
+++ Array_2D.hpp
@@ -0,0 +1,17 @@
+//
+// Created by maciej on 12.07.25.
+//
+
+#ifndef ARRAY_2D_HPP
+#define ARRAY_2D_HPP
+#include <vector>
+
+template <typename Type>
+class Array_2d {
+public:
+
+};
+
+
+
+#endif //ARRAY_2D_HPP
