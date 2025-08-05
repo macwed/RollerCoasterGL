5a153580e24630eaef86552e1b39d306618f6c9b
Maciej Wędzik <logmaciej@gmail.com>
2025-08-05 19:56:12 +0200

    Add DrawableMixin template base class

---
+++ DrawableMixin.hpp
@@ -0,0 +1,20 @@
+//
+// Created by maciej on 06.08.25.
+//
+
+#ifndef DRAWABLEMIXIN_HPP
+#define DRAWABLEMIXIN_HPP
+
+
+template <typename T>
+class DrawableMixin {
+    public:
+    void drawImpl()
+    {
+        static_cast<T*>(this)->draw();
+    }
+};
+
+
+
+#endif //DRAWABLEMIXIN_HPP
